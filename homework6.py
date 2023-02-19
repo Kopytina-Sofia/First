2634ee23ae0c37570d78e016c95980e811469146
Sofia Kopytina <sofikokopytina@gmail.com>
2023-02-19 22:07:41 +0200

    Homework6

---
+++ homework6.py
@@ -0,0 +1,14 @@
+import colorama
+from colorama import Fore
+from colorama import Back
+
+
+print(Fore.RED + Back.BLACK +"Мене звуть Софія")
+print(Fore.BLUE + "Моє призвище Копитіна")
+print(Fore.GREEN + Back.LIGHTWHITE_EX + "Мені 14 років")
+print(Fore.CYAN + "Народилася в місті Миколаїв")
+print(Fore.LIGHTGREEN_EX + Back.BLACK + "Вчуся в Комп'ютерній Академії ШАГ")
+print(Fore.LIGHTMAGENTA_EX + "В группі 1911")
+print(Fore.YELLOW + Back.LIGHTWHITE_EX + "У вільний час люблю дивитися фільми та малювати")
+
+
